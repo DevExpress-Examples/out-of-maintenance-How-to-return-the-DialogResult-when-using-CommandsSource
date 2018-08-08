3cf9f0383ca8cccc415ba141a92031fe3af0a599
Sameishchev Anton <anton.sameishchev@devexpress.com>
2018-08-08 13:55:07 +0300

    code example added

---
+++ CS/MainWindowViewModel.cs
@@ -0,0 +1,23 @@
+﻿using DevExpress.Mvvm;
+using DevExpress.Mvvm.ModuleInjection;
+using System.Windows.Input;
+
+namespace DialogTest
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public MainWindowViewModel()
+        {
+            ShowDialogCommand = new DelegateCommand(OnShowDialog);
+        }
+
+        public ICommand ShowDialogCommand { get; set; }
+
+        public async void OnShowDialog()
+        {
+            var injectionResult = await ModuleManager.DefaultWindowManager.RegisterOrShowOrActivate("DialogView",
+                                           new Module("MyDialogView", () => new MyDialogViewModel(), typeof(MyDialogView)));
+            System.Diagnostics.Debug.WriteLine(injectionResult.Result);
+        }
+    }
+}
---
+++ CS/MyDialogViewModel.cs
@@ -0,0 +1,58 @@
+﻿using DevExpress.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DialogTest
+{
+    public interface IDialogViewModel
+    {
+        int DialogWidth { get; set; }
+        int DialogHeight { get; set; }
+        string DialogTitle { get; set; }
+        IEnumerable<UICommand> CommandsSource { get; set; }
+    }
+
+    public class MyDialogViewModel : IDialogViewModel
+    {
+        public MyDialogViewModel()
+        {
+            DialogTitle = "Print dialog";
+            DialogWidth = 640;
+            DialogHeight = 480;
+            CommandsSource = new List<UICommand>
+            {
+                new UICommand
+                {
+                    Id = System.Windows.MessageBoxResult.Cancel,
+                    Caption = "Cancel",
+                    IsCancel = true,
+                    Tag =  System.Windows.MessageBoxResult.Cancel
+
+                },
+                new UICommand
+                {
+                    Caption = "Print",
+                    IsDefault = true,
+                    Command = new DelegateCommand(Print),
+                    Tag = System.Windows.MessageBoxResult.OK
+                }
+            };
+        }
+
+        #region IDialogViewModel
+        public int DialogWidth { get; set; }
+        public int DialogHeight { get; set; }
+        public string DialogTitle { get; set; }
+        public IEnumerable<UICommand> CommandsSource { get; set; }
+        #endregion IDialogViewModel
+
+        private void Print()
+        {
+
+        }
+
+    }
+}
