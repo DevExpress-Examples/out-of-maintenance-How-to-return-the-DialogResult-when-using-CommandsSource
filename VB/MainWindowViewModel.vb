2060ae1e1dc6a1765120c7f881edab5ecbea3714
DevExpressExampleBot <fakeEmail>
2018-08-08 17:34:18 +0300

    Source auto update [skip ci]

---
+++ VB/MainWindowViewModel.vb
@@ -0,0 +1,20 @@
+﻿Imports DevExpress.Mvvm
+Imports DevExpress.Mvvm.ModuleInjection
+Imports System.Windows.Input
+
+Namespace DialogTest
+    Public Class MainWindowViewModel
+        Inherits ViewModelBase
+
+        Public Sub New()
+            ShowDialogCommand = New DelegateCommand(AddressOf OnShowDialog)
+        End Sub
+
+        Public Property ShowDialogCommand() As ICommand
+
+        Public Async Sub OnShowDialog()
+            Dim injectionResult = Await ModuleManager.DefaultWindowManager.RegisterOrShowOrActivate("DialogView", New System.Reflection.Module("MyDialogView", Function() New MyDialogViewModel(), GetType(MyDialogView)))
+            System.Diagnostics.Debug.WriteLine(injectionResult.Result)
+        End Sub
+    End Class
+End Namespace
---
+++ VB/MyDialogViewModel.vb
@@ -0,0 +1,51 @@
+﻿Imports DevExpress.Mvvm
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Threading.Tasks
+
+Namespace DialogTest
+    Public Interface IDialogViewModel
+        Property DialogWidth() As Integer
+        Property DialogHeight() As Integer
+        Property DialogTitle() As String
+        Property CommandsSource() As IEnumerable(Of UICommand)
+    End Interface
+
+    Public Class MyDialogViewModel
+        Implements IDialogViewModel
+
+        Public Sub New()
+            DialogTitle = "Print dialog"
+            DialogWidth = 640
+            DialogHeight = 480
+            CommandsSource = New List(Of UICommand) From { _
+                New UICommand With { _
+                    .Id = System.Windows.MessageBoxResult.Cancel, _
+                    .Caption = "Cancel", _
+                    .IsCancel = True, _
+                    .Tag = System.Windows.MessageBoxResult.Cancel _
+                }, _
+                New UICommand With { _
+                    .Caption = "Print", _
+                    .IsDefault = True, _
+                    .Command = New DelegateCommand(AddressOf Print), _
+                    .Tag = System.Windows.MessageBoxResult.OK _
+                } _
+            }
+        End Sub
+
+        #Region "IDialogViewModel"
+        Public Property DialogWidth() As Integer Implements IDialogViewModel.DialogWidth
+        Public Property DialogHeight() As Integer Implements IDialogViewModel.DialogHeight
+        Public Property DialogTitle() As String Implements IDialogViewModel.DialogTitle
+        Public Property CommandsSource() As IEnumerable(Of UICommand) Implements IDialogViewModel.CommandsSource
+        #End Region ' IDialogViewModel
+
+        Private Sub Print()
+
+        End Sub
+
+    End Class
+End Namespace
